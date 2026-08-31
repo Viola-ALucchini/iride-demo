9a0608b1049301bf31051c18658022188d9cae57
Viola-ALucchini <126567665+Viola-ALucchini@users.noreply.github.com>
2026-08-31 15:41:10 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const C='iride-v10';
-const A=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./icon-maskable-512.png','./apple-touch-icon.png'];
+const C='iride-v12';
+const A=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./icon-maskable-512.png','./apple-touch-icon.png','./xlsx.full.min.js','./dati.xlsx'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(A.map(u=>new Request(u,{cache:'reload'})))).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))).then(()=>self.clients.claim()));});
 self.addEventListener('fetch',e=>{
@@ -8,6 +8,11 @@ self.addEventListener('fetch',e=>{
   if(isPage){
     e.respondWith(fetch(new Request(r,{cache:'no-store'})).then(res=>{ const cp=res.clone(); caches.open(C).then(c=>{c.put('./index.html',cp.clone());c.put('./',cp);}); return res; })
       .catch(()=>caches.match('./index.html')));
+    return;
+  }
+  if(url.pathname.endsWith('/dati.xlsx')){
+    e.respondWith(fetch(new Request(r,{cache:'no-store'})).then(res=>{ const cp=res.clone(); caches.open(C).then(c=>c.put('./dati.xlsx',cp)); return res; })
+      .catch(()=>caches.match('./dati.xlsx')));
     return;
   }
   e.respondWith(caches.match(r).then(x=>x||fetch(r)));
